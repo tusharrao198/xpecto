cc5b2dd1732bc95161bc67c69edf0801093a9714
BeingHomosapien <rohanraj378@gmail.com>
2021-05-01 16:20:15 +0530

    Sound/Curor

---
+++ js/sound.js
@@ -0,0 +1,27 @@
+window.onload = () => {
+    document.querySelector(".anthem").play()
+}
+
+let sound = document.querySelector(".sound");
+let status = document.querySelector("#status");
+sound.addEventListener("click", () => {
+
+    let audios = document.getElementsByTagName("audio")
+        // audios[0].play();
+    console.log(audios)
+    if (status.innerText == "ON") {
+        Array.from(audios).forEach((audio) => {
+            audio.muted = true;
+        })
+        status.innerText = "OFF";
+    } else {
+        Array.from(audios).forEach((audio) => {
+            audio.muted = false;
+        })
+        status.innerText = "ON";
+    }
+})
+
+document.addEventListener("click", () => {
+    document.getElementById("click").play();
+})
