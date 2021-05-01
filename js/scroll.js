3374788dc1ec8f09681ceb33262b6fb8523352bf
BeingHomosapien <rohanraj378@gmail.com>
2021-05-01 21:59:50 +0530

    Added Preloader

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,8 @@
 let a = $("#enter")
 a.on('click', () => {
+    document.querySelector(".click-sound").play();
+    document.querySelector(".anthem").play();
     $(".preloader").css({ "transform": "translateX(-100%)" })
     $(".home").css({ "opacity": 1, "transform": "translateY(0)", })
+
 })
