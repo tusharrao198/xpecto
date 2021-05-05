bb9388bfc994ca4384e5b865ce824d07212b79f0
Divyasheel <dk0018w@gmail.com>
2021-05-05 12:44:59 +0530

    redesigned landing page

--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,7 @@
-/*===== EXPANDER MENU  =====*/ 
-const showMenu = (toggleId, navId)=>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
-  
-    if(toggle && nav){
-      toggle.addEventListener('click', ()=>{
-        nav.classList.toggle('show')
-        toggle.classList.toggle('bx-x')
-      })
-    }
-  }
-  showMenu('header-toggle','nav-menu')
+const menuToggle = document.querySelector(".toggle");
+const showcase = document.querySelector(".showcase");
 
-  /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
-
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  showcase.classList.toggle("active");
+});
