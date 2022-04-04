3fe27c32aa39737c82df23f33bce89031de5440e
Tushar Rao <tushar.rao198@gmail.com>
2022-04-04 10:32:00 +0530

    Added Users db Schema for mongodb. storing userinfo in mongodb refactor project structure

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    googleId: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    displayName: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    teamId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "team",
+    },
+    image: {
+        type: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("UserDetails", UserSchema);
