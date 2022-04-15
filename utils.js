b5d891acb684459735d248208ec44928e7ae3ed9
lazyXghost <rjtbnslrealme@gmail.com>
2022-04-15 23:41:41 +0530

    Added create team form functionality

--- utils.js
+++ utils.js
@@ -9,7 +9,11 @@ module.exports = {
         const event = await eventTable.findOne({ name: params.event }).lean();
         return event;
     },
-
+    findEventFromId: async function(event_id) {
+        const eventTable = require("./models/Events");
+        const event = await eventTable.findOne({ _id: event_id }).lean();
+        return event;
+    },
     findUserTeam: async function (req) {
         const event = await module.exports.findEvent(req);
         const teamTable = require("./models/Team");
