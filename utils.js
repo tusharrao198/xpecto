05550364f25959f31aa60a75424e65e27021462f
lazyXghost <rjtbnslrealme@gmail.com>
2022-04-17 14:38:11 +0530

    Added join team

--- utils.js
+++ utils.js
@@ -17,9 +17,11 @@ module.exports = {
     findUserTeam: async function (req) {
         const event = await module.exports.findEvent(req);
         const teamTable = require("./models/Team");
-        const team = await teamTable
-            .findOne({ event: event._id, teamLeader: req.user._id })
-            .lean();
+
+        var team = await teamTable.findOne({ event: event._id, teamLeader: req.user._id }).lean();
+        if(team == null)
+            team = await teamTable.findOne({ members: req.user._id }).lean();
+
         return team;
     },
     findUserTeamFromId: async function(req){
@@ -44,6 +46,20 @@ module.exports = {
                 return err;
             }
         });
+    },
+    joinTeam: async function(req){
+        const formDetails = req.body;
+        const inviteCodeTable = require("./models/InviteCode");
+        const inviteCode = await inviteCodeTable.findOne({ code: formDetails.invite_code }).lean();
+        if(inviteCode != null){
+            const teamTable = require("./models/Team");
+            await teamTable.updateOne(
+                { _id: inviteCode.team },
+                { $push: { members: req.user._id }}
+            );
+            const team = await teamTable.findOne({_id: inviteCode.team}).lean();
+            console.log(team);
+        }
     },
     generateString: async function(length) {
         let result = '';
