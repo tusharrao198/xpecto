90255c3a57cf7eec6629ce720e89eb932d4b0f1b
dipesh809 <b20096@students.iitmandi.ac.in>
2022-04-21 18:20:17 +0530

    removed sound feature

--- static/js/sound.js
+++ static/js/sound.js
@@ -1,26 +1,26 @@
-// window.onload = () => {
-//     document.querySelector(".anthem").play()
-// }
+// // window.onload = () => {
+// //     document.querySelector(".anthem").play()
+// // }
 
-let sound = document.querySelector(".sound");
-let status = document.querySelector("#status");
-sound.addEventListener("click", () => {
-    let audios = document.getElementsByTagName("audio");
-    // audios[0].play();
-    // console.log(audios)
-    if (status.innerText == "ON") {
-        Array.from(audios).forEach((audio) => {
-            audio.muted = true;
-        });
-        status.innerText = "OFF";
-    } else {
-        Array.from(audios).forEach((audio) => {
-            audio.muted = false;
-        });
-        status.innerText = "ON";
-    }
-});
+// let sound = document.querySelector(".sound");
+// let status = document.querySelector("#status");
+// sound.addEventListener("click", () => {
+//     let audios = document.getElementsByTagName("audio");
+//     // audios[0].play();
+//     // console.log(audios)
+//     if (status.innerText == "ON") {
+//         Array.from(audios).forEach((audio) => {
+//             audio.muted = true;
+//         });
+//         status.innerText = "OFF";
+//     } else {
+//         Array.from(audios).forEach((audio) => {
+//             audio.muted = false;
+//         });
+//         status.innerText = "ON";
+//     }
+// });
 
-document.addEventListener("click", () => {
-    document.getElementById("click").play();
-});
+// document.addEventListener("click", () => {
+//     document.getElementById("click").play();
+// });
