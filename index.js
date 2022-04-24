46a952ea84ae094391cede3ddd3bdf2c2b72d8a9
Eagle <b20086@students.iitmandi.ac.in>
2022-04-24 22:20:35 +0530

    google auth added

---
+++ index.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const express = require("express");
+const port = process.env.PORT || 5000;
+const passport = require("passport");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const { authCheck } = require("./middleware/auth");
+const authRoutes = require("./routes/authroutes");
+const connectDB = require("./config/db");
+var url = require("url");
+
+// Load config
+require("dotenv").config({ path: "./config/config.env" });
+
+// Passport Config
+require("./config/passport")(passport);
+
+// connect to Database
+connectDB();
+
+const app = express();
+
+// Configure bodyParser
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+// set template view engine
+app.set("views", "./templates");
+app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + "/static"));
+app.use("/images", express.static(__dirname + "static/images"));
+
+app.use(function (req, res, next) {
+    if (!req.user) {
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+        res.header('Expires', '-1');
+        res.header('Pragma', 'no-cache');
+    }
+    next();
+});
+
+app.use('/xpecto.ico', express.static('../static/images/xpecto.ico'));
+// Sessions middleware
+app.use(
+    session({
+        secret: process.env.SECRET,
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.MONGO_DATABASE_URI }),
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+// Routes
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
+    // console.log(req.session.user)
+    res.render("index", {
+        authenticated: req.isAuthenticated()
+    });
+});
+
+app.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`Connection Established!! http://localhost:${port}`);
+});
