a9753bfe15d58c89705d51f225379175e0669be7
Tushar Rao <tushar.rao198@gmail.com>
2022-05-11 08:52:54 +0530

    added ps button to be live dynamically thorugh db

--- models/code.js
+++ models/code.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
-
+// this schema was to generate referral codes for campus ambassadors
+// Not used right now
 const CodeSchema = new mongoose.Schema({
-    campus_ambassador_name: {
-        type: String,
-        required: true,
-    },
-    place: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    code: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    used: {
-        type: Number,
-        required: true,
-    },
+	campus_ambassador_name: {
+		type: String,
+		required: true,
+	},
+	place: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	code: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	used: {
+		type: Number,
+		required: true,
+	},
 });
 
 module.exports = mongoose.model("code", CodeSchema);
